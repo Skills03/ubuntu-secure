7c0c535d0b594bfa6b085491a79be84ac46ea380
Skills03 <126560833+Skills03@users.noreply.github.com>
2025-10-01 13:00:11 +0530

    new

--- ubuntu-secure-master/complete_syscall_blockchain.c
+++ ubuntu-secure-master/complete_syscall_blockchain.c
@@ -23,10 +23,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdarg.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <sys/mman.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 #include <errno.h>
 #include <fcntl.h>
